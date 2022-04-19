2e1c283dab36b33cb83ac4802264df50ecaac4ff
Stas Paun <paunstas@gmail.com>
2022-04-19 15:46:09 +0300

    change background header

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,6 +30,12 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader(){
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 
 /*==================== SHOW SCROLL UP ====================*/ 
